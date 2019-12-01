5f62aa704a072009bf624340b97fb263ccc309df
mihal09 <mjanik1@op.pl>
2019-12-01 15:18:59 +0100

    gui changes

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,4 +1,41 @@
 package sample;
 
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        final MainController mainController = new MainController();
+
+        Button button = new Button("Odwiez plansze");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                System.out.println("odwiezam");
+                mainController.drawBoard();
+            }
+        });
+        HBox hBox = new HBox(20, mainController.board, button);
+        mainController.drawBoard();
+        primaryStage.setTitle("Go");
+        primaryStage.setScene(new Scene(hBox));
+        primaryStage.setMaximized(false);
+        primaryStage.setResizable(true);
+        primaryStage.setMinHeight(768);
+        primaryStage.setMinWidth(1024);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
