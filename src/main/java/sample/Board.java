e18a06bb18bb18ac069e4806084947ea28878f9d
mihal09 <mjanik1@op.pl>
2019-12-01 15:18:13 +0100

    added board class

---
+++ src/main/java/sample/Board.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class Board extends BoardAbstract {
+    public static final int offset = 40;
+    public Board(){
+        size = 10;
+        addStones();
+    }
+
+    public void addStones(){
+        stones = new Stone[size][size];
+        // creating fields
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                stones[i][j] = new Stone(i*(Stone.radius+offset)+Stone.radius, j*(Stone.radius+offset)+Stone.radius, ColorEnum.EMPTY);
+            }
+        }
+    }
+}
