64d3c29727ce89242a1e7488091428c263ad44f3
mihal09 <romolka99@gmail.com>
2019-12-07 22:46:27 +0100

    board tests

--- src/test/java/server/BoardTest.java
+++ src/test/java/server/BoardTest.java
@@ -1,6 +1,7 @@
 package server;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -25,5 +26,32 @@ public class BoardTest {
         board.isMoveLegal(0,0,ColorEnum.BLACK);
         Assert.assertEquals(board.getGroups().size(),1);
 
+    }
+
+    @Test
+    public void isInsideBoard() {
+        int size =4;
+        Board board = new Board(size);
+        Assert.assertEquals(board.isInsideBoard(0,0), true);
+        Assert.assertEquals(board.isInsideBoard(3,3), true);
+        Assert.assertEquals(board.isInsideBoard(-1,0), false);
+        Assert.assertEquals(board.isInsideBoard(0,4), false);
+        Assert.assertEquals(board.isInsideBoard(4,4), false);
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void isSameColor() {
+    }
+
+    @Test
+    public void getField() {
+    }
+
+    @Test
+    public void findGroupWithStone() {
     }
 }
